8b09ac04a13c46b555a24e5bf15621bc085b38a6
kalmansour <63953899+kalmansour@users.noreply.github.com>
2022-04-07 03:59:17 +0300

    madlibs function intro statement, user variable inputs and story

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,17 @@
 def main():
-	# write your code here
+	print("Mad libs where libs get mad.")
+	print("Start below:\n")
 
+	time = input('Enter a number from 1 to 12:')
+	items = input('Enter a noun (plural):')
+	name = input('Enter a name:').title()
+	scream = input('Enter any sentence:').upper()
+	action = input('Enter a verb:')
+	story = f"It was {time} o'clock when I heard a knock at the door."f"\nI opened the door and there was a box full of {items} with a note saying \"From {name}\"."f"\nJust as I closed the door I heard a scream \"{scream}.\""f"\nI froze in place and all I could do was {action}."
 
+	print("\nThe story goes...\n")
+
+	print(story)
 
 if __name__ == '__main__':
 	main()
